b99b5dcee709661d60f293a103acba25329dbd97
Sascha Gärtner <sascha.gaertner@hdi.de>
2022-03-25 13:36:19 +0100

    adding toString methods to entities

--- src/main/java/org/salex/hmip/observer/data/Sensor.java
+++ src/main/java/org/salex/hmip/observer/data/Sensor.java
@@ -73,4 +73,13 @@ public class Sensor {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+    @Override
+    public String toString() {
+        return "Sensor{" +
+                "name='" + name + '\'' +
+                ", type=" + type +
+                ", sgtin='" + sgtin + '\'' +
+                '}';
+    }
 }
