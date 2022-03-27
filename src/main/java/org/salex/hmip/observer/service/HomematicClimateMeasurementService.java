c54fe7b833a5af873788fed16767be77cacee799
Sascha Gärtner <sascha.gaertner@hdi.de>
2022-03-27 17:04:44 +0200

    refactoring sensor management

--- src/main/java/org/salex/hmip/observer/service/HomematicClimateMeasurementService.java
+++ src/main/java/org/salex/hmip/observer/service/HomematicClimateMeasurementService.java
@@ -12,28 +12,26 @@ import java.util.Map;
 public class HomematicClimateMeasurementService implements ClimateMeasurementService {
     private final HmIPClient client;
 
-    private final ObserverDatabase database;
+    private final Map<String, Sensor> sensors = new HashMap<>();
 
     public HomematicClimateMeasurementService(HmIPClient client, ObserverDatabase database) {
         this.client = client;
-        this.database = database;
+        for(var sensor : database.getSensors()) {
+            sensors.put(sensor.getSgtin().replace("-", ""), sensor);
+        }
     }
 
     @Override
     public Mono<Reading> measureClimateValues(Reading reading) {
-        final var sensorMap = new HashMap<String, Sensor>();
-        for(var sensor : this.database.getSensors()) {
-            sensorMap.put(sensor.getSgtin().replace("-", ""), sensor);
-        }
         return client.loadCurrentState()
                 .map(HmIPState::getDevices)
                 .map(Map::values)
                 .flatMapMany(Flux::fromIterable)
-                .filter(device -> sensorMap.containsKey(device.getSGTIN()))
+                .filter(device -> this.sensors.containsKey(device.getSGTIN()))
                 .map(device -> reading.addMeasurement(device.getChannels().values().stream()
                             .filter(HmIPState.ClimateSensorChannel.class::isInstance)
                             .map(HmIPState.ClimateSensorChannel.class::cast)
-                            .map(channel -> new ClimateMeasurement(reading, sensorMap.get(device.getSGTIN()), device.getStatusTimestamp(), channel.getTemperature(), (double) channel.getHumidity(), channel.getVaporAmount()))
+                            .map(channel -> new ClimateMeasurement(reading, sensors.get(device.getSGTIN()), device.getStatusTimestamp(), channel.getTemperature(), (double) channel.getHumidity(), channel.getVaporAmount()))
                             .map(Measurement.class::cast)
                             .findFirst()))
                 .then(Mono.just(reading));
