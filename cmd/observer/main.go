4b4b7cd379adae835ad638d86c8b591729fc407f
Sascha Gärtner <sascha.gaertner@hdi.de>
2023-02-01 16:23:38 +0100

    undo changes for encryption

--- cmd/observer/main.go
+++ cmd/observer/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	goenv "github.com/caitlinelfring/go-env-default"
 	"github.com/salex-org/smart-home-observer/internal/controller"
-	"golang.org/x/term"
 	"gopkg.in/yaml.v2"
 	"log"
 	"net/http"
@@ -29,36 +27,14 @@ type MQTTConfiguration struct {
 
 func main() {
 	rawConfig, err := os.ReadFile("/config/observer-config.yml")
-	var decryptedConfig []byte
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	decryptConfig := goenv.GetBoolDefault("CONFIG_DECRYPTION_ENABLED", true)
-
-	if decryptConfig {
-		var key string
-		for {
-			fmt.Printf("Enter config key: ")
-			buffer, err := term.ReadPassword(int(os.Stdin.Fd()))
-			if err == nil {
-				key = string(buffer)
-				if len(key) != 0 {
-					break
-				}
-			}
-		}
-		fmt.Println("\nDecrypting configuration...")
-		// TODO Decrypting with 'key' using https://astaxie.gitbooks.io/build-web-application-with-golang/content/en/09.6.html
-	} else {
-		fmt.Println("Decrypting configuration skipped")
-		decryptedConfig = rawConfig
-	}
-
 	var config Configuration
 
-	err = yaml.Unmarshal(decryptedConfig, &config)
+	err = yaml.Unmarshal(rawConfig, &config)
 	if err != nil {
 		fmt.Println(err)
 		return
