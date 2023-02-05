007403a8f36ef1afd96341aa7cf82a0895210cce
Sascha Gärtner <sascha.gaertner@salex.org>
2023-02-05 17:08:04 +0100

    fixing bugs

--- docker/prod/decrypt-config.sh
+++ docker/prod/decrypt-config.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec -it smart-home-observer-1 /smart-home-observer -i docker/local/observer/config/observer-config.yml -o docker/local/observer/config/observer-config.decrypted.yml decrypt-config
+docker exec -it smart-home-observer-1 /smart-home-observer -i config/observer-config.yml -o config/observer-config.decrypted.yml decrypt-config
--- docker/prod/encrypt-config.sh
+++ docker/prod/encrypt-config.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec -it smart-home-observer-1 /smart-home-observer -o docker/local/observer/config/observer-config.yml -i docker/local/observer/config/observer-config.decrypted.yml encrypt-config
+docker exec -it smart-home-observer-1 /smart-home-observer -o config/observer-config.yml -i config/observer-config.decrypted.yml encrypt-config
