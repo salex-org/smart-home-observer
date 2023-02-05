0f8d8c2d0bc082f8d4b76c6184ac454330564e91
Sascha Gärtner <sascha.gaertner@salex.org>
2023-02-05 17:56:37 +0100

    adding install script and restart after config encryption

--- docker/prod/encrypt-config.sh
+++ docker/prod/encrypt-config.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 docker exec -it smart-home-observer-1 /smart-home-observer -o config/observer-config.yml -i config/observer-config.decrypted.yml encrypt-config
+docker restart smart-home-observer-1
+docker attach smart-home-observer-1
---
+++ docker/prod/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+version=$1
+docker compose --file ./docker-compose.yml --project-name smart-home down
+curl -LO https://github.com/salex-org/smart-home-observer/releases/download/v$version/scripts.tar.gz
+sudo tar xfvz scripts.tar.gz
+rm scripts.tar.gz
+docker compose --file ./docker-compose.yml --project-name smart-home up --detach
+docker attach smart-home-observer-1
